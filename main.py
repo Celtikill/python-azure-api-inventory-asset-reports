7ade22cccbdabb9a6e99520c2958a3ec335e46d4
Celtikill <celtikill@celtikill.io>
2024-05-16 22:02:49 +0000

    http 200 on externalized config

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import sys
 import os
 import json
@@ -26,6 +28,7 @@ try:
     LOGIN_URL = "https://login.microsoftonline.com/"+os.environ.get('ARM_TENANT_ID')+"/oauth2/token"
     PARAMS_MGMT = {'grant_type':'client_credentials','client_id': ''+os.environ.get('ARM_CLIENT_ID')+'','client_secret':''+os.environ.get('ARM_CLIENT_SECRET')+'','resource':'https://management.azure.com/'}
     login = requests.post(url = LOGIN_URL, data = PARAMS_MGMT)
+    print(login.status_code)
     jsonfy = login.json()
     token = jsonfy["access_token"]
     query_header = {"Content-Type": "application/json", "Authorization": "Bearer "+token}
